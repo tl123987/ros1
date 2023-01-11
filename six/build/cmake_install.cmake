f576b76b5ac77a761883ac8dbff9cd94259a3c96
westsky <65206700+tl123987@users.noreply.github.com>
2023-01-11 21:14:13 +0800

    gesture

--- six/build/cmake_install.cmake
+++ six/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/media/y/Elements/ubuntu/ros resoure/ros1/six/build/gtest/cmake_install.cmake")
+  include("/media/y/Elements/ubuntu/ros resoure/ros1/six/build/gesture/cmake_install.cmake")
 
 endif()
 
